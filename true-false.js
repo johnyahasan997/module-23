389d48c7716e773733f3d91b2f8289391b2d6f63
Johny Ahasan <johnyahasan2030@gmail.com>
2021-02-02 01:13:24 +0600

    true/false file

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+//false
+
+//Truthy
+//'0', ' ', [], {}
+let name = NaN;
+console.log(name);
+if (name) {
+  console.log("condition is true.");
+} else {
+  console.log("condition is false.");
+}
