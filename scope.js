614a5275710dfbc3e452643b8dc953b1a8c96a61
Johny Ahasan <johnyahasan2030@gmail.com>
2021-02-02 23:59:10 +0600

    Add code

---
+++ scope.js
@@ -0,0 +1,20 @@
+let bonus = 20;
+
+function sum(first, second) {
+  let result = first + second + bonus;
+  // console.log(bonus);
+  if (result > 9) {
+    var mood = "happy";
+    mood = "fishy";
+    mood = 'funky';
+    mood = 'cranky'
+    // console.log(mood);
+  }
+  console.log(day);
+  let day = "friday";
+  return result;
+}
+
+const sumResult = sum(20, 30);
+// console.log(bonus);
+// console.log(sumResult);
